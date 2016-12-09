6659db5a240fd19d14e85fe234ad635084fc3f2e
David Levy <levydav@gmail.com>
2016-12-09 08:31:18 +0200

    Move router to a seperate class

---
+++ System/Router.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace System;
+
+class Router {
+
+    private $server, $request, $get, $post;
+    private $defaultController = 'home';
+    private $defaultAction = 'index';
+
+    function __construct(array $server, array $request, $get, $post) {
+        $this->server = $server;
+        $this->request = $request;
+        $this->get = $get;
+        $this->post = $post;
+    }
+
+    function __invoke() {
+        $uri_parts = parse_url($this->server['REQUEST_URI']);
+        $parts = explode('/', $uri_parts['path']);
+
+        array_shift($parts);
+
+        if ($parts && ! $parts[0]) {
+            $parts = [];
+        }
+
+        switch (count($parts)) {
+            case 0:
+                $controllerName = $this->defaultController;
+                $actionName = $this->defaultAction;
+                break;
+            case 1:
+                $controllerName = array_shift($parts);
+                $actionName = $this->defaultAction;
+                break;
+            default:
+                $controllerName = array_shift($parts);
+                $actionName = array_shift($parts);
+                break;
+        }
+
+        $controllerClass = 'Controller\\' . ucfirst(strtolower($controllerName)) . 'Controller';
+
+        if ( ! class_exists($controllerClass) || ! method_exists($controllerClass, $actionName)) {
+            return false;
+        }
+        $controller = new $controllerClass;
+
+        $controller->$actionName($parts);
+    }
+
+}
--- index.php
+++ index.php
@@ -1,55 +1,11 @@
 <?php
-spl_autoload_register('__autoloader') ;
-$uri_parts = parse_url($_SERVER['REQUEST_URI']);
-//echo
-json_encode(
-        [
-            'Server' => $_SERVER,
-            'Uri' => $uri_parts
-        ]
-);
-//exit;
-$defaultController = 'home';
-$defaultAction = 'index';
 
-$parts =  explode('/', $uri_parts['path']);
+spl_autoload_register('__autoloader');
 
-array_shift($parts);
+$router = new System\Router($_SERVER, $_REQUEST, $_GET, $_POST);
 
-if($parts && !$parts[0]){
-    $parts = [];
+$router();
+
+function __autoloader($class_name) {
+    include str_replace('\\', '/', $class_name) . '.php';
 }
-
-switch (count($parts)) {
-    case 0:
-        $controllerName = $defaultController;
-        $action = $defaultAction;
-        break;
-    case 1:
-        $controllerName = array_shift($parts);
-//        $controllerName = $controllerName ? $controllerName : $defaultController;
-        $action = $defaultAction;
-        break;
-    default:
-        $controllerName = array_shift($parts);
-        $action = array_shift($parts);
-        break;
-}
-
-$controllerClass = 'Controller\\'.ucfirst(strtolower($controllerName)) . 'Controller';
-
-$controller = new $controllerClass;
-
-$controller->$action($parts);
-
-
-
-function __autoloader($class_name){
-    include str_replace('\\', '/', $class_name).'.php';
-}
-
-
-
-
-
-
