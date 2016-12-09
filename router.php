36f8446d0957984ea335ca0c6a819b2c82a3db27
David Levy <levydav@gmail.com>
2016-12-09 07:43:05 +0200

    add html to roter.php

--- router.php
+++ router.php
@@ -1,6 +1,5 @@
 <?php
-chdir('httpdocs');
-if (preg_match('/\.(?:js|png|jpg|jpeg|gif|css|woff|ttf|eot|svg|otf)/',  strtolower($_SERVER["REQUEST_URI"]))) {
+if (preg_match('/\.(?:js|png|jpg|jpeg|gif|css|woff|ttf|eot|svg|otf|html)/',  strtolower($_SERVER["REQUEST_URI"]))) {
     return false;    // serve the requested resource as-is.
 } else {
     include 'index.php';
